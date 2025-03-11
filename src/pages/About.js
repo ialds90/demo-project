952d845b3cf0f431a67685da515dd1ed069999c4
ialds90 <indranath.alds@gmail.com>
2025-03-11 21:38:31 +0530

    Add task list display to About page using AppContext

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,18 @@
-import React, { useState } from "react";
+import React, { useContext,useState } from "react";
+import { AppContext } from "../context/AppContext";
 
-const About = () => {
+const About = () => { 
+  const { tasks, setTasks } = useContext(AppContext);
 
 
   return (
     <div>
       <h1>About Page</h1>
+      <ul>
+        {tasks.map((tasks, index) => (
+          <li key={index}>{tasks}</li>
+        ))}
+      </ul>
     </div>
   );
 };
