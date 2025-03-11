7e737edffd76145751b34273d75af4fcbface8fc
ialds90 <indranath.alds@gmail.com>
2025-03-11 21:47:42 +0530

    Refactor routing in App component by creating AppRoutes for better organization

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
 import React from "react";
-
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-
-import Home from "./pages/Home";
-import About from "./pages/About";
-import NotFound from "./pages/NotFound";
+import { BrowserRouter as Router, Link } from "react-router-dom";
+import AppRoutes from "./routes/AppRoutes";
 
 const App = () => {
   return (
@@ -12,11 +8,7 @@ const App = () => {
       <nav>
         <Link to="/">Home</Link> | <Link to="/about">About</Link>
       </nav>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+      <AppRoutes />
     </Router>
   );
 };
